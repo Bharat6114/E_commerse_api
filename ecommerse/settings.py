985c6e4fc0d341c1b563506306ee2e69b21374ae
Bharat6114 <bharatbhandari5123@gmail.com>
2022-11-27 22:13:10 +0545

    first commit

--- ecommerse/settings.py
+++ ecommerse/settings.py
@@ -117,11 +117,32 @@ WSGI_APPLICATION = 'ecommerse.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ecommerse',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+        
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+    #     'NAME': '',
+
+    #     'USER': '',
+
+    #     'PASSWORD': '9848826114',
+
+    #     'HOST': 'localhost:8000',
+
+    #     'PORT': '5432',
+
+    # }
+
+}}
 
 
 # Password validation
